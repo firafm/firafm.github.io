59120c5abd3f808abe3219fe134ee701f7285fe6
fira <firamulia_19@kharisma.ac.id>
2021-02-15 21:55:59 +0800

    lat10

--- latihan10/js/api.js
+++ latihan10/js/api.js
@@ -1,22 +1,16 @@
-//script untuk menangani proses response dari proses request API
 function status(response) {
   if (response.status !== 200) {
-    console.log("Error : " + response.status);
-    // Method reject() akan membuat blok catch terpanggil
+    console.log("Error: "+response.status);
     return Promise.reject(new Error(response.statusText));
   } else {
-    // Mengubah suatu objek menjadi Promise agar bisa "di-then-kan"
     return Promise.resolve(response);
   }
 }
 
-// Blok kode untuk mem-parsing json menjadi array JavaScript
-function json(response) {
+function json (response) {
   return response.json();
 }
 
-// Blok kode untuk meng-handle kesalahan di blok catch
 function error(error) {
-  // Parameter error berasal dari Promise.reject()
-  console.log("Error : " + error);
+  console.log("Error: "+error);
 }
